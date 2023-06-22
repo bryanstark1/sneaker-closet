dc0ec0ab0459f13df1c3499ab0b61c653857359e
bryanstark1 <bryandstark@icloud.com>
2023-06-22 11:22:36 -0400

    Created object properties in sneaker model

--- models/sneaker.js
+++ models/sneaker.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 
 const sneakerSchema = new Schema({
   model: String,
-
+  name: String,
+  color: String,
+  year: Date,
+  retail: Number,
+  image: String,
+  sku: String
 });
 
 module.exports = mongoose.model('Sneakers', sneakerSchema);
